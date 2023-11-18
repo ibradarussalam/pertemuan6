1d1b7b5b138f60fe23b57f36636f0af585049a5a
ibradarussalam <aibiareigaming@gmail.com>
2023-11-17 20:57:53 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const nilai = prompt('masukkan nilai')
+if (nilai > 90) {
+    alert ('A')
+
+  } else if (nilai > 80) {
+    alert ('B+')
+
+  } else if (nilai > 70) {
+    alert ('B')
+
+  } else {
+    alert ('REMEDIAL')
+  }
+
+alert ('BLOKKKKKKK')
+
+
+
+
+
